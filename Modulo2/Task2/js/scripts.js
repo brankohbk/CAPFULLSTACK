3f07f8ce32622eb10d01e4d68c87a128caf3e184
brankohbk <brankoh@gmail.com>
2019-04-17 20:24:30 -0300

    .

--- Modulo2/Task2/js/scripts.js
+++ Modulo2/Task2/js/scripts.js
@@ -18,30 +18,49 @@ function llenarTabla(datos) {
   `;
 document.getElementById("table-rows").innerHTML = markup; //INSERTO TODOS LOS ROWS EN EL ELEMENTO table-rows.
 
-
-// OTRA FORMA DE LLENAR LA TABLA
-
-// var str = ``; // DECLARO E INICIALIZO LA CADENA str QUE SE VA A USAR PARA EL innerHTML.
-// for (let index = 0; index < miembros.length; index++) {
-//   const miembro = miembros[index];
-//   str += `<tr>
-//     <td><a href="${miembro.url}">${miembro.first_name} ${miembro.middle_name || ''} ${miembro.last_name}</a></td>
-//     <td>${miembro.party}</td>
-//     <td>${miembro.state}</td>
-//     <td>${miembro.seniority}</td>
-//     <td>${miembro.votes_with_party_pct} &percnt;</td>
-//     </tr>
-//     `;
-
-
-// }
-
-// document.getElementById("table-rows").innerHTML = str;
     }
+
+
 
 function llenarDropdownEstados(datos){
   const miembros = datos.results[0].members;
   let estados = miembros.map(miembro => miembro.state);
-  return estados;
+  let estadosUnicos = [...new Set(estados)].sort(); // "..."= el resto de los elementos, "new Set()"=es un valor único del array pasado por parametro. 
+ 
+  const markup = 
+    ` <button class="dropdown-item" id="stateButton">All States</button>
+      ${estadosUnicos.map(estado => 
+        `<button class="dropdown-item" id="stateButton" type="button" value="${estado}">${estado}</button>
+        `).join('')
+      }
+  `;
+document.getElementById("stateDropdown").innerHTML = markup; 
+ 
+}
+
+function partyFilter() {  
+
+  let checkedBoxes = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected => selected.value);
+  let rows = Array.from(document.getElementById('table-rows').querySelectorAll('tr'));
+    
+  for (let index = 0; index < rows.length; index++) {
+    rows[index].style.display='';
+    
+  }  
+  
+  for (let index = 0; index < rows.length; index++) {
+    
+    let row = rows[index];
+    let cell = row.getElementsByTagName('td')[1];
+    let party = cell.textContent || cell.innerText;
+     
+      if (party.toUpperCase().indexOf() > -1) {
+            
+      } else{
+        row.style.display='none';
+      }
+        
+      }
+      
 
 }
