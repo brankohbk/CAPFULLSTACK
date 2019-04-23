31f584f15490cd01c6f338759aadbf8b999bbd2c
brankohbk <brankoh@gmail.com>
2019-04-23 11:10:56 -0300

    checkboxes FIXED

---
+++ Modulo2/Task2/js/scripts.1.js
@@ -0,0 +1,96 @@
+"use strict"
+// FILTRADOS OCULTANDO FILAS. NOOOOOO ES LO CORRECTO
+// ===============================
+// AÑADO LOS eventListener.
+var allCheckboxes = Array.from(document.querySelectorAll('input[name=party]'));
+allCheckboxes.map(checkbox => checkbox.addEventListener('change', partyFilter));
+
+// var allButtons = Array.from(document.querySelectorAll('button[class="dropdown-item"]'));
+// allButtons.map(button => button.addEventListener('clic', stateFilter(button.value)));
+
+// ===============================
+// GENERA TABLA Y DROPDOWN DE ESTADOS.
+function renderComponents(rawData) {
+  var members = rawData.results[0].members; //APUNTO AL ARRAY members DENTRO DE results DEL JSON.
+
+  llenarDropdownEstados(members);
+  llenarTabla(members);
+
+}
+
+// ===============================
+// LLENO LA TABLA CON INFORMACION.
+
+function llenarTabla(miembros) {
+
+  // USANDO TEMPLATE STRINGS Y ARROW FUNCTIONS.
+
+  const markup = //INICIALIZO LA VARIABLE QUE CONTENDRÁ EL STRING HTML DEL ROW.
+    ` 
+      ${miembros.map(miembro => //POR CADA miembro EN EL ARRAY miembros GENERO UN ROW CON LA INFORMACION DE ESE MIEMBRO ESPECIFICO.
+        `<tr class="party${miembro.party.toUpperCase()} state${miembro.state.toUpperCase()}">
+        <td><a href="${miembro.url}" target=_blank >${miembro.first_name} ${miembro.middle_name || ''} ${miembro.last_name}</a></td>
+        <td>${miembro.party}</td>
+        <td>${miembro.state}</td>
+        <td>${miembro.seniority}</td>
+        <td>${miembro.votes_with_party_pct} &percnt;</td>
+        </tr>`
+        ).join('')//ELIMINO LAS COMAS DEL ARRAY DEVUELTO POR EL .map .
+      }
+  `;
+document.getElementById("table-rows").innerHTML = markup; //INSERTO TODOS LOS ROWS EN EL ELEMENTO table-rows.
+    }
+
+// ===============================
+// LLENO EL DROPDOWN CON LOS ESTADOS.
+
+function llenarDropdownEstados(miembros){
+  // const miembros = datos.results[0].members;
+  let estados = miembros.map(miembro => miembro.state);
+  let estadosUnicos = [...new Set(estados)].sort(); // "..."= el resto de los elementos, "new Set()"=es un valor único del array pasado por parametro. 
+ 
+  const markup = 
+    ` <button class="dropdown-item" id="stateButton">All States</button>
+      ${estadosUnicos.map(estado => 
+        `<button class="dropdown-item" id="stateButton" type="button" value="${estado}">${estado}</button>
+        `).join('')
+      }
+  `;
+document.getElementById("stateDropdown").innerHTML = markup; 
+ 
+}
+
+// ===============================
+// FILTRO POR PARTIDO (checkboxes).
+
+function partyFilter() {  
+
+  let checkedBoxes = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected =>`party${selected.value.toUpperCase()}`);
+  let rows = Array.from(document.getElementById('table-rows').querySelectorAll('tr'));
+  rows.forEach(row => {row.classList.remove("partyDisplay"); row.classList.add("partyDisplayNone");}); //RESET DE CLASES DE DISPLAY DE LAS rows.
+
+  // EN CADA row PREGUNTA POR CADA VALOR DEL ARRAY DE CHECKEADOS.
+  
+  rows.forEach(row => { checkedBoxes.forEach(checked => {
+                    if (Array.from(row.classList).includes(checked)) {
+                      // SI ESTÁ CHECKEADO, AGREGAR CLASE PARA MOSTRAR Y BORRAR CLASE PARA OCULTAR
+                      row.classList.add("partyDisplay");
+                      row.classList.remove("partyDisplayNone");                      
+                    }
+                    else if (!Array.from(row.classList).includes("partyDisplay")){
+                      // SI NO FUE EXPLICITAMENTE MOSTRADA MEDIANTE CHECKBOX, OCULTAR LA row                    
+                      row.classList.add("partyDisplayNone");
+
+                    }
+                }
+              )
+            } 
+          );
+
+}
+function stateFilter(valor) {
+
+  console.log(valor);
+
+  
+}
--- Modulo2/Task2/js/scripts.js
+++ Modulo2/Task2/js/scripts.js
@@ -1,53 +1,73 @@
+"use strict"
+
+// ===============================
+// SELECCIONO EL ORIGEN DE LOS DATOS EN CRUDO. SI houseData NO ESTÁ DEFINIDO, USA senateData.
+var rawData = typeof houseData !== 'undefined' ? houseData : senateData;
+var members = rawData.results[0].members; //APUNTO AL ARRAY members DENTRO DE results DEL JSON.
+
+// ===============================
 // AÑADO LOS eventListener.
-
 var allCheckboxes = Array.from(document.querySelectorAll('input[name=party]'));
-allCheckboxes.map(checkbox => checkbox.addEventListener('change', partyFilter));
-
-var allButtons = Array.from(document.querySelectorAll('button[class="dropdown-item"]'));
-allButtons.map(button => button.addEventListener('clic', stateFilter(button.value)));
+allCheckboxes.forEach(checkbox => checkbox.addEventListener('change', renderComponents));
 
 // ===============================
 // GENERA TABLA Y DROPDOWN DE ESTADOS.
-function renderComponents(datos) {
+function renderComponents() {
+  var checkedBoxes = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected => selected.value.toUpperCase());
 
-  llenarDropdownEstados(datos);
-  llenarTabla(datos);
+  llenarDropdownEstados(members);
+
+  llenarTabla(partyFilter(checkedBoxes, members), "table-rows");
 
 }
 
 // ===============================
+// FILTRO POR PARTIDO (checkboxes).
+
+function partyFilter(parties, memberlist) {
+  let filtered = [];
+  let aux = [];
+  parties.forEach(party => {
+    aux = memberlist.filter(member => member.party == party);
+    filtered.push(...aux);
+  })
+
+  return filtered;
+}
+
+
+
+// ===============================
 // LLENO LA TABLA CON INFORMACION.
 
-function llenarTabla(datos) {
-  const miembros = datos.results[0].members; //APUNTO AL ARRAY members DENTRO DE results DEL JSON.
+function llenarTabla(miembros, elementoHTML) {
 
-  // USANDO TEMPLATE STRINGS Y ARROW FUNCTIONS.
+  //   // USANDO TEMPLATE STRINGS Y ARROW FUNCTIONS.
 
   const markup = //INICIALIZO LA VARIABLE QUE CONTENDRÁ EL STRING HTML DEL ROW.
     ` 
-      ${miembros.map(miembro => //POR CADA miembro EN EL ARRAY miembros GENERO UN ROW CON LA INFORMACION DE ESE MIEMBRO ESPECIFICO.
-        `<tr class="party${miembro.party.toUpperCase()} state${miembro.state.toUpperCase()}">
-        <td><a href="${miembro.url}" target=_blank >${miembro.first_name} ${miembro.middle_name || ''} ${miembro.last_name}</a></td>
-        <td>${miembro.party}</td>
-        <td>${miembro.state}</td>
-        <td>${miembro.seniority}</td>
-        <td>${miembro.votes_with_party_pct} &percnt;</td>
-        </tr>`
-        ).join('')//ELIMINO LAS COMAS DEL ARRAY DEVUELTO POR EL .map .
-      }
-  `;
-document.getElementById("table-rows").innerHTML = markup; //INSERTO TODOS LOS ROWS EN EL ELEMENTO table-rows.
-    }
+        ${miembros.map(miembro => //POR CADA miembro EN EL ARRAY miembros GENERO UN ROW CON LA INFORMACION DE ESE MIEMBRO ESPECIFICO.
+          `<tr>
+          <td><a href="${miembro.url}" target=_blank >${miembro.first_name} ${miembro.middle_name || ''} ${miembro.last_name}</a></td>
+          <td>${miembro.party}</td>
+          <td>${miembro.state}</td>
+          <td>${miembro.seniority}</td>
+          <td>${miembro.votes_with_party_pct} &percnt;</td>
+          </tr>`
+          ).join('')//ELIMINO LAS COMAS DEL ARRAY DEVUELTO POR EL .map .
+        }
+    `;
+  document.getElementById(elementoHTML).innerHTML = markup; //INSERTO TODOS LOS ROWS EN EL ELEMENTO table-rows.
 
+      }
 // ===============================
 // LLENO EL DROPDOWN CON LOS ESTADOS.
 
-function llenarDropdownEstados(datos){
-  const miembros = datos.results[0].members;
+function llenarDropdownEstados(miembros) {
   let estados = miembros.map(miembro => miembro.state);
   let estadosUnicos = [...new Set(estados)].sort(); // "..."= el resto de los elementos, "new Set()"=es un valor único del array pasado por parametro. 
- 
-  const markup = 
+
+  const markup =
     ` <button class="dropdown-item" id="stateButton">All States</button>
       ${estadosUnicos.map(estado => 
         `<button class="dropdown-item" id="stateButton" type="button" value="${estado}">${estado}</button>
@@ -56,39 +76,4 @@ function llenarDropdownEstados(datos){
   `;
 document.getElementById("stateDropdown").innerHTML = markup; 
  
-}
-
-// ===============================
-// FILTRO POR PARTIDO (checkboxes).
-
-function partyFilter() {  
-
-  let checkedBoxes = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected =>`party${selected.value.toUpperCase()}`);
-  let rows = Array.from(document.getElementById('table-rows').querySelectorAll('tr'));
-  rows.forEach(row => {row.classList.remove("partyDisplay"); row.classList.add("partyDisplayNone");}); //RESET DE CLASES DE DISPLAY DE LAS rows.
-
-  // EN CADA row PREGUNTA POR CADA VALOR DEL ARRAY DE CHECKEADOS.
-  
-  rows.forEach(row => { checkedBoxes.forEach(checked => {
-                    if (Array.from(row.classList).includes(checked)) {
-                      // SI ESTÁ CHECKEADO, AGREGAR CLASE PARA MOSTRAR Y BORRAR CLASE PARA OCULTAR
-                      row.classList.add("partyDisplay");
-                      row.classList.remove("partyDisplayNone");                      
-                    }
-                    else if (!Array.from(row.classList).includes("partyDisplay")){
-                      // SI NO FUE EXPLICITAMENTE MOSTRADA MEDIANTE CHECKBOX, OCULTAR LA row                    
-                      row.classList.add("partyDisplayNone");
-
-                    }
-                }
-              )
-            } 
-          );
-
-}
-function stateFilter(valor) {
-
-  console.log(valor);
-
-  
 }
