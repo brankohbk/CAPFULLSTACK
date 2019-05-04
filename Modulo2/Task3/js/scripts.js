1a2cfe04d2a5469a4c37b5ae86f4a74681394622
brankohbk <brankoh@gmail.com>
2019-05-03 21:56:19 -0300

    count members by party, votes by party

--- Modulo2/Task3/js/scripts.js
+++ Modulo2/Task3/js/scripts.js
@@ -18,14 +18,23 @@ window.addEventListener("scroll", myScrollFunc);
 
 
 // ===============================
-// SELECCIONO EL ORIGEN DE LOS DATOS EN CRUDO. SI houseData NO ESTÁ DEFINIDO, USA senateData.
+// SELECCIONO EL ORIGEN DE LOS DATOS EN CRUDO.
+// Si houseData NO ESTÁ DEFINIDO, USA senateData.
 var rawData = typeof houseData !== 'undefined' ? houseData : typeof senateData !== 'undefined' ? senateData : "";
 var members = "";
-if (rawData !== "") { members = rawData.results[0].members; } //APUNTO AL ARRAY members DENTRO DE results DEL JSON.
+//APUNTO AL ARRAY members DENTRO DE results DEL JSON.
+if (rawData !== "") { members = rawData.results[0].members; }
+// Inicializo los estados seleccionados y busco las abreviaciones en el diccionario.
+var ddmenu = document.querySelector('#stateDropdown');
 var state = "";
 var actualState = "All States";
 var dictionary = "";
 if (typeof abbreviations !== 'undefined') { dictionary = abbreviations[0]; }
+//Genero las listas de miembros de cada partido. 
+var democrats = [];
+var independents = [];
+var republicans = [];
+
 
 // ===============================
 // AÑADO LOS eventListener.
@@ -34,20 +43,23 @@ allCheckboxes.forEach(checkbox => checkbox.addEventListener('change', renderComp
 
 // ===============================
 // LLENA EL Dropdown CON LOS ESTADOS
-llenarDropdownEstados(members);
+llenarDropdownEstados(members, "stateDropdown");
 
 // ===============================
 // GENERA TABLA Y ESCONDE DROPDOWN DE ESTADOS.
 function renderComponents() {
-
-  document.getElementById("state").innerHTML = actualState; //ESCRIBE EL NOMBRE DEL ESTADO AL LADO DEL dropdown
-
-  var ddmenu = document.querySelector('#stateDropdown');
-  ddmenu.classList.remove('show');
-
+  if (document.getElementById("state") !== null) {
+    //ESCRIBE EL NOMBRE DEL ESTADO SELECCIONADO EN EL div CORRESPONDIENTE.
+    document.getElementById("state").innerHTML = actualState;
+  }
+  if (ddmenu !== null) {
+    ddmenu.classList.remove('show');
+  }
   var checkedBoxes = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected => selected.value.toUpperCase());
   llenarTabla(partyFilter(checkedBoxes, stateFilter(state, members)), "table-rows");
+
 }
+
 
 // ===============================
 // SELECCIONA EL ESTADO
@@ -106,8 +118,8 @@ function llenarTabla(miembros, elementoHTML) {
 
   const markup = //INICIALIZO LA VARIABLE QUE CONTENDRÁ EL STRING HTML DEL ROW.
     ` 
-        ${miembros.map(miembro => //POR CADA miembro EN EL ARRAY miembros GENERO UN ROW CON LA INFORMACION DE ESE MIEMBRO ESPECIFICO.
-          `<tr>
+  ${miembros.map(miembro => //POR CADA miembro EN EL ARRAY miembros GENERO UN ROW CON LA INFORMACION DE ESE MIEMBRO ESPECIFICO.
+    `<tr>
           <td><a href="${miembro.url}" target=_blank >${miembro.last_name}, ${miembro.first_name} ${miembro.middle_name || ''}</a></td>
           <td>${miembro.party}</td>
           <td>${miembro.state}</td>
@@ -117,24 +129,58 @@ function llenarTabla(miembros, elementoHTML) {
           ).join('')//ELIMINO LAS COMAS DEL ARRAY DEVUELTO POR EL .map .
         }
     `;
-  document.getElementById(elementoHTML).innerHTML = markup; //INSERTO TODOS LOS ROWS EN EL ELEMENTO table-rows.
+    if (document.getElementById(elementoHTML) !== null){
+      document.getElementById(elementoHTML).innerHTML = markup; //INSERTO TODOS LOS ROWS EN EL ELEMENTO table-rows.
+    }
+    
+  }
 
-      }
-
-// ===============================
-// LLENO EL DROPDOWN CON LOS ESTADOS.
-function llenarDropdownEstados(miembros) {
+  // ===============================
+  // LLENO EL DROPDOWN CON LOS ESTADOS.
+  function llenarDropdownEstados(miembros,elementoHTML) {
   let estados = miembros.map(miembro => miembro.state);
   let estadosUnicos = [...new Set(estados)].sort(); // "..."= el resto de los elementos, "new Set()"=es un valor único del array pasado por parametro. 
   let fullState="";
   // SE AGREGÓ DICCIONARIO DE ABREVIACIONES DE LOS ESTADOS.
   let markup =` <button class="dropdown-item" id="stateButton" onclick="selectState(this.value)" value="">All States</button>`;
-   estadosUnicos.forEach(estado => {
-     fullState= abbreviations.filter(element => element.abbreviation == estado).map(element => element.name);
-     markup += `<button class="dropdown-item" id="stateButton" type="button" onclick="selectState(this.value)" value="${estado},${fullState}">${fullState}</button>`
-    } 
-     
-        );
+  estadosUnicos.forEach(estado => {
+    fullState= abbreviations.filter(element => element.abbreviation == estado).map(element => element.name);
+    markup += `<button class="dropdown-item" id="stateButton" type="button" onclick="selectState(this.value)" value="${estado},${fullState}">${fullState}</button>`
+  } 
+  
+  );
+  if (document.getElementById(elementoHTML) !== null){
+  document.getElementById(elementoHTML).innerHTML = markup; //DIBUJA EL dropdown
+}
+}
 
-document.getElementById("stateDropdown").innerHTML = markup; //DIBUJA EL dropdown
+// ===============================
+// CUENTO LA CANTIDAD DE MIEMBROS POR PARTIDO
+function contar() {  
+  statistics[0].number_of_democrats = democrats.length;
+  statistics[0].number_of_independents = independents.length;
+  statistics[0].number_of_republicans = republicans.length;
+  statistics[0].total= democrats.length + independents.length + republicans.length;
+}
+
+// ===============================
+// CALCULAR VOTOS CON EL PARTIDO.
+function votesParty(array) {
+  let average = array.reduce(function (acum,value) {
+    return (acum + value.votes_with_party_pct);
+  },0)/array.length;
+  return average.toFixed(2);
+}
+
+// ===============================
+// FUNCIONES DE ESTADISTICAS.
+function allStatistics(array) {
+  //Genero las listas de miembros de cada partido. 
+  democrats = array.filter(member => member.party === "D");
+  independents = array.filter(member => member.party === "I");
+  republicans = array.filter(member => member.party === "R");
+  contar();
+  statistics[0].votes_with_party_democrats = votesParty(democrats);
+  statistics[0].votes_with_party_independents = votesParty(independents);
+  statistics[0].votes_with_party_republicans = votesParty(republicans);
 }
---
+++ Modulo2/Task3/js/statistics.js
@@ -0,0 +1,9 @@
+var statistics = [{
+  "number_of_democrats": 0,
+  "number_of_independents": 0,
+  "number_of_republicans": 0,
+  "total": 0,
+  "votes_with_party_democrats": 0,
+  "votes_with_party_independents": 0,
+  "votes_with_party_republicans": 0
+}];
