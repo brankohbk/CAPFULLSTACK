c87a370a2db9f564bbfe5cbe747896e2d191a5ac
brankohbk <brankoh@gmail.com>
2019-05-06 22:05:52 -0300

    .

--- Modulo2/Task3/js/statistics.js
+++ Modulo2/Task3/js/statistics.js
@@ -1,4 +1,4 @@
-let statistics = [{
+var statistics = {
   "number_of_democrats": 0,
   "number_of_independents": 0,
   "number_of_republicans": 0,
@@ -6,28 +6,29 @@ let statistics = [{
   "votes_with_party_democrats": 0,
   "votes_with_party_independents": 0,
   "votes_with_party_republicans": 0
-}];
+};
 
 function renderTables() {
   allStatistics(members);
-  renderPartyVotes(statistics[0], 'partyVotesTable');
+  renderPartyVotes(statistics, 'partyVotesTable');
 }
 
 // ===============================
 // CUENTO LA CANTIDAD DE MIEMBROS POR PARTIDO
 function count() {
-  statistics[0].number_of_democrats = democrats.length;
-  statistics[0].number_of_independents = independents.length;
-  statistics[0].number_of_republicans = republicans.length;
-  statistics[0].total = democrats.length + independents.length + republicans.length;
+  statistics.number_of_democrats = democrats.length;
+  statistics.number_of_independents = independents.length;
+  statistics.number_of_republicans = republicans.length;
+  statistics.total = democrats.length + independents.length + republicans.length;
 }
 
 // ===============================
 // CALCULAR VOTOS CON EL PARTIDO.
 function votesParty(array) {
-  const average = array.reduce(function(acum, value) {
+  let average = array.reduce(function(acum, value) {
     return (acum + value.votes_with_party_pct);
   }, 0) / array.length;
+  average ? average : average = 0;
   return average.toFixed(2);
 }
 
@@ -38,10 +39,13 @@ function allStatistics(array) {
   democrats = array.filter(member => member.party === "D");
   independents = array.filter(member => member.party === "I");
   republicans = array.filter(member => member.party === "R");
+
   count();
-  statistics[0].votes_with_party_democrats = votesParty(democrats);
-  statistics[0].votes_with_party_independents = votesParty(independents);
-  statistics[0].votes_with_party_republicans = votesParty(republicans);
+  statistics.votes_with_party_democrats = votesParty(democrats);
+  statistics.votes_with_party_independents = votesParty(independents);
+  statistics.votes_with_party_republicans = votesParty(republicans);
+
+  leastLoyal(array);
 }
 
 function renderPartyVotes(data, htmlElement) {
@@ -64,4 +68,13 @@ function renderPartyVotes(data, htmlElement) {
   if (htmlElement !== 'null') {
     document.getElementById(htmlElement).innerHTML = markup;
   };
+}
+
+function leastLoyal(data) {
+
+  let sorted = data.sort((x, y) => { return x.votes_with_party_pct - y.votes_with_party_pct; });
+  let leastLoyalLimit = (sorted.length) / .10;
+  // console.log(sorted.length);
+  // return sorted;
+
 }
