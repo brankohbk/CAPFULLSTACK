172579ec22402cb2a1ea3067e333fc3c2e0dd2d7
brankohbk <brankoh@gmail.com>
2019-05-31 20:38:26 -0300

    .

--- Modulo2/Task4/js/app.js
+++ Modulo2/Task4/js/app.js
@@ -1,38 +1,45 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        members: [],
-        selectedParties: ['D', 'I', 'R'],
-        selectedState: '',
-        states: [],
-        abbs: abbreviations,
-        actualState: 'All States',
-        selectedState: 'All States',
+  el: '#app',
+  data: {
+    members: [],
+    selectedParties: ['D', 'I', 'R'],
+    selectedState: '',
+    states: [],
+    abbs: abbreviations,
+    actualState: 'All States',
+    selectedState: 'All States',
 
+  },
+  methods: {
+    partyFilter: function(member) {
+      if (this.selectedParties.includes(member.party)) {
+        return true;
+      }
     },
-    methods: {
-        partyFilter: function(member) {
-            if (this.selectedParties.includes(member.party)) {
-                return true;
-            }
-        },
-        partySelect: function() {
-            this.selectedParties = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected => selected.value.toUpperCase());
-        },
-        stateFilter: function(member) {
-            if (this.selectedState === 'All States') {
-                return true;
-            } else if (member.state === this.selectedState) {
-                return true;
-            }
-        },
-        selectState: function(select) {
-            this.selectedState = select.abbreviation;
-            this.actualState = select.name;
-        },
+    partySelect: function() {
+      this.selectedParties = Array.from(document.querySelectorAll('input[name=party]:checked')).map(selected => selected.value.toUpperCase());
+    },
+    stateFilter: function(member) {
+      if (this.selectedState === 'All States') {
+        return true;
+      } else if (member.state === this.selectedState) {
+        return true;
+      }
+    },
+    selectState: function(select) {
+      this.selectedState = select.abbreviation;
+      this.actualState = select.name;
+    },
 
 
+  },
+  computed: {
+    emptyParties: function() {
+      if (this.selectedParties.length === 0) {
+        return 'highlight';
+      }
     }
+  }
 
 });
 
@@ -43,13 +50,13 @@ const promiseModifiers = { headers: { 'X-API-Key': '9TpSPs9WWEJazoq0YrySUhmSOrnl
 
 let dataPromise = fetch(promiseURL, promiseModifiers);
 dataPromise
-    .then(response => response.json())
-    .then(myJson => {
-        app.members = myJson.results[0].members;
+  .then(response => response.json())
+  .then(myJson => {
+    app.members = myJson.results[0].members;
 
-        // Filtra los estados del JSON para armar el dropdown.
-        let estados = app.members.map(miembro => miembro.state);
-        app.states = [...new Set(estados)].sort();
+    // Filtra los estados del JSON para armar el dropdown.
+    let estados = app.members.map(miembro => miembro.state);
+    app.states = [...new Set(estados)].sort();
 
-    })
-    .catch(err => { `Couldn't retrieve info. please, check this error: ${err}` });
+  })
+  .catch(err => { `Couldn't retrieve info. please, check this error: ${err}` });
