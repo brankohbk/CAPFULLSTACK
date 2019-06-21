5ec84ddb4365c5c8102cac93c14de0aed6573d31
brankohbk <brankoh@gmail.com>
2019-06-21 20:27:36 -0300

    .

--- Modulo3/Task2Git/README.txt
+++
@@ -1,3 +0,0 @@
-TASK 2 IMPLEMENTANDO VUE Y BOOTSTRAP
-
-no era lo que se pedia en los submits
--- Modulo3/Task2Git/js/app.js
+++
@@ -1,9 +0,0 @@
-let nysl = new Vue({
-  el: '#app',
-  data: {
-    title: 'Please, select your team from below'
-  },
-  methods: {},
-  computed: {}
-
-});
