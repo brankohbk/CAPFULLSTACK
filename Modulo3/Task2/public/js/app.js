5921f7f3f90c6b8aa18c795f345aa6eeb8770ca4
brankohbk <brankoh@gmail.com>
2019-06-23 18:44:26 -0300

    funcion de filtro global

--- Modulo3/Task2/public/js/app.js
+++ Modulo3/Task2/public/js/app.js
@@ -2,109 +2,134 @@ let nysl = new Vue({
   el: '#app',
   data: {
     matches: [{
-        "date": "1/9/2019",
-        "teams": ['U1', 'U4'],
+        "date": "2019-09-01",
+        "teamA": "U1",
+        "teamB": "U4",
         "location": "AJ Katzenmaier",
         "time": "9:30 a.m."
       },
       {
-        "date": "1/9/2019",
-        "teams": ['U3', 'U2'],
+        "date": "2019-09-01",
+        "teamA": "U3",
+        "teamB": "U2",
         "location": "Greenbay",
         "time": "1:00 p.m."
       },
       {
-        "date": "8/9/2019",
-        "teams": ['U5', 'U6'],
+        "date": "2019-09-08",
+        "teamA": "U5",
+        "teamB": "U6",
         "location": "Howard A Yeager",
         "time": "9:30 a.m."
       },
       {
-        "date": "8/9/2019",
-        "teams": ['U6', 'U1'],
+        "date": "2019-09-08",
+        "teamA": "U6",
+        "teamB": "U1",
         "location": "Marjorie P Hart",
         "time": "1:00 p.m."
       },
       {
-        "date": "15/9/2019",
-        "teams": ['U2', 'U4'],
+        "date": "2019-09-15",
+        "teamA": "U2",
+        "teamB": "U4",
         "location": "North",
         "time": "9:30 a.m."
       },
       {
-        "date": "15/9/2019",
-        "teams": ['U3', 'U5'],
+        "date": "2019-09-15",
+        "teamA": "U3",
+        "teamB": "U5",
         "location": "AJ Katzenmaier",
         "time": "1:00 p.m."
       },
       {
-        "date": "22/9/2019",
-        "teams": ['U1', 'U3'],
+        "date": "2019-09-22",
+        "teamA": "U1",
+        "teamB": "U3",
         "location": "South",
         "time": "9:30 a.m."
       },
       {
-        "date": "22/9/2019",
-        "teams": ['U2', 'U6'],
+        "date": "2019-09-22",
+        "teamA": "U2",
+        "teamB": "U6",
         "location": "Howard A Yeager",
         "time": "1:00 p.m."
       },
       {
-        "date": "29/9/2019",
-        "teams": ['U4', 'U5'],
+        "date": "2019-09-29",
+        "teamA": "U4",
+        "teamB": "U5",
         "location": "Greenbay",
         "time": "9:30 a.m."
       },
       {
-        "date": "6/10/2019",
-        "teams": ['U2', 'U5'],
+        "date": "2019-10-06",
+        "teamA": "U2",
+        "teamB": "U5",
         "location": "Marjorie P Hart",
         "time": "9:30 a.m."
       },
       {
-        "date": "6/10/2019",
-        "teams": ['U1', 'U6'],
+        "date": "2019-10-06",
+        "teamA": "U1",
+        "teamB": "U6",
         "location": "South",
         "time": "1:00 p.m."
       },
       {
-        "date": "13/10/2019",
-        "teams": ['U3', 'U4'],
+        "date": "2019-10-13",
+        "teamA": "U3",
+        "teamB": "U4",
         "location": "Howard A Yeager",
         "time": "9:30 a.m."
       },
       {
-        "date": "13/10/2019",
-        "teams": ['U5', 'U1'],
+        "date": "2019-10-13",
+        "teamA": "U5",
+        "teamB": "U1",
         "location": "Greenbay",
         "time": "1:00 p.m."
       },
       {
-        "date": "20/10/2019",
-        "teams": ['U6', 'U3'],
+        "date": "2019-10-20",
+        "teamA": "U6",
+        "teamB": "U3",
         "location": "North",
         "time": "9:30 a.m."
       },
       {
-        "date": "20/10/2019",
-        "teams": ['U2', 'U4'],
+        "date": "2019-10-20",
+        "teamA": "U2",
+        "teamB": "U4",
         "location": "Marjorie P Hart",
         "time": "1:00 p.m."
       },
       {
-        "date": "27/10/2019",
-        "teams": ['U3', 'U1'],
+        "date": "2019-10-27",
+        "teamA": "U3",
+        "teamB": "U1",
         "location": "AJ Katzenmaier",
         "time": "9:30 a.m."
       },
       {
-        "date": "27/10/2019",
-        "teams": ['U5', 'U6'],
+        "date": "2019-10-27",
+        "teamA": "U5",
+        "teamB": "U6",
+        "location": "Howard A Yeager",
+        "time": "1:00 p.m."
+      },
+      {
+        "date": "2019-06-23",
+        "teamA": "U5",
+        "teamB": "U6",
         "location": "Howard A Yeager",
         "time": "1:00 p.m."
       }
     ],
     title: 'Please select your team',
+    filter: '',
     filteredMatches: [],
     infoContainerClasslist: 'col-12 justify-content-center',
   },
@@ -119,40 +144,33 @@ let nysl = new Vue({
     showTeams: function() {
       this.hideShowDivs("teams");
       this.title = 'Please select your team';
-    },
-    teamFilter: function(team) {
-      this.filteredMatches = this.matches.filter(match => (match.teams[0] === team || match.teams[1] === team));
-      this.hideShowDivs("filtered");
-      this.title = "Matches for team " + team;
-    },
-    dateFilter: function(date) {
-      this.filteredMatches = this.matches.filter(match => date === match.date);
-      this.hideShowDivs("filtered");
-      this.title = "Matches scheduled on " + date;
-    },
-    locationFilter: function(location) {
-
-      this.filteredMatches = this.matches.filter(match => location === match.location);
-      this.hideShowDivs("filtered");
-      this.title = "Matches played on " + location + " stadium";
-
+      this.filter = '';
     },
 
-    globalFilter: function(attribute, param) {
+    globalFilter: function(value) {
+      if (typeof(value) === 'undefined') {
+        this.filteredMatches = this.matches;
+      } else {
 
-      this.filteredMatches = this.matches.filter(
-
-      );
-
+        this.filteredMatches = this.matches.filter(
+          match => {
+            for (const key in match) {
+              if (match[key] === value) {
+                return true;
+              }
+            }
+          }
+        );
+      }
+      this.title = "Filtered by";
+      this.filter = value;
       this.hideShowDivs("filtered");
-      console.log(this.filteredMatches);
-
     },
 
   },
   computed: {
     teams: function() {
-      let aux = this.matches.map(match => match.teams[0] && match.teams[1]);
+      let aux = this.matches.map(match => match.teamA || match.teamB);
       let teams = [...new Set(aux)].sort();
       return teams;
     },
