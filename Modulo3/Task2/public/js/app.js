eae92f798fe28ce1991a9c8c765192f6502925b2
brankohbk <brankoh@gmail.com>
2019-07-10 19:35:13 -0300

    portrait iframe

--- Modulo3/Task2/public/js/app.js
+++ Modulo3/Task2/public/js/app.js
@@ -131,27 +131,37 @@ let nysl = new Vue({
     locations: {
       "AJ Katzenmaier": {
         "map": "https://www.google.com/maps?ll=41.900292,-87.62905&z=14&t=m&hl=en-US&gl=AR&mapclient=embed&q=24+W+Walton+St+Chicago,+IL+60610+USA",
-        "address": "24 W. Walton St., Chicago, IL 60610"
+        "address": "24 W. Walton St., Chicago, IL 60610",
+        "embeded": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23757.23410997335!2d-87.62905!3d41.900292!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f6bac3%3A0x68a82e5d59952c86!2s24+W+Walton+St%2C+Chicago%2C+IL+60610%2C+USA!5e0!3m2!1sen!2sar!4v1562798048490!5m2!1sen!2sar",
       },
       "Greenbay": {
         "map": "https://maps.google.com/maps?ll=41.913802,-87.637839&z=14&t=m&hl=en-US&gl=AR&mapclient=embed&q=1734%20N%20Orleans%20St%20Chicago%2C%20IL%2060614%20USA",
-        "address": "1734 N. Orleans St., Chicago, IL 60614"
+        "address": "1734 N. Orleans St., Chicago, IL 60614",
+        "embeded": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23752.207178859142!2d-87.637839!3d41.913802!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34073f306a3%3A0x9e1726bbf8f23f0e!2s1734+N+Orleans+St%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2sar!4v1562797694973!5m2!1sen!2sar",
       },
       "Howard A Yeager": {
         "map": "https://www.google.com/maps/place/2245+N+Southport+Ave,+Chicago,+IL+60614,+USA/@41.923265,-87.662926,14z/data=!4m5!3m4!1s0x880fd2e37f9b8d2d:0x62ad8b907dd755d6!8m2!3d41.9232646!4d-87.6629259?hl=en-US",
-        "address": "2245 N. Southport Ave., Chicago, IL 60614"
+        "address": "2245 N. Southport Ave., Chicago, IL 60614",
+        "embeded": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.585683085616!2d-87.66511458455746!3d41.92326457921871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f9b8d2d%3A0x62ad8b907dd755d6!2s2245+N+Southport+Ave%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2sar!4v1562797784947!5m2!1sen!2sar",
+
       },
       "Marjorie P Hart": {
         "map": "https://maps.google.com/maps?ll=41.929578,-87.645898&z=14&t=m&hl=en-US&gl=AR&mapclient=embed&q=2625%20N%20Orchard%20St%20Chicago%2C%20IL%2060614%20USA",
-        "address": "2625 N. Orchard St., Chicago, IL 60614"
+        "address": "2625 N. Orchard St., Chicago, IL 60614",
+        "embeded": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23746.33542120715!2d-87.645898!3d41.929578!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2630e551%3A0x3e719e44a5cef714!2s2625+N+Orchard+St%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2sar!4v1562797843970!5m2!1sen!2sar",
+
       },
       "North": {
         "map": "https://maps.google.com/maps?ll=41.907062,-87.646275&z=16&t=m&hl=en-US&gl=US&mapclient=embed",
-        "address": "1409 N. Ogden Ave., Chicago, IL 60610"
+        "address": "1409 N. Ogden Ave., Chicago, IL 60610",
+        "embeded": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.337799844479!2d-87.64837162030375!3d41.90709647932166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd33af0e6ccc3%3A0x26c81c1d557667da!2s1409+N+Ogden+Ave%2C+Chicago%2C+IL+60610!5e0!3m2!1sen!2sus!4v1562797920037!5m2!1sen!2sus",
+
       },
       "South": {
         "map": "https://maps.google.com/maps?ll=41.919777,-87.651367&z=14&t=m&hl=en-US&gl=AR&mapclient=embed&q=2101%20N%20Fremont%20St%20Chicago%2C%20IL%2060614%20USA",
-        "address": "2101 N. Fremont St., Chicago, IL 60614"
+        "address": "2101 N. Fremont St., Chicago, IL 60614",
+        "embeded": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23749.983522029845!2d-87.651367!3d41.919777!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3196fb41dc7%3A0x970be7f7d6336df5!2s2101+N+Fremont+St%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2sar!4v1562797953368!5m2!1sen!2sar",
+
       },
     },
     titles: {
@@ -168,6 +178,7 @@ let nysl = new Vue({
     actualCategory: '',
     filteredMatches: [],
     categoryData: '',
+    selectedStadium: "",
     showDiv: 'home',
     infoContainerClasslist: 'col-12 justify-content-center',
     history: [],
@@ -228,6 +239,9 @@ let nysl = new Vue({
       }
       this.showSection("filtered");
     },
+    selectStadium: function(selection) {
+      this.selectedStadium = selection;
+    }
   },
   computed: {
     // Los equipos son extraídos de todos los partidos y pasados a un array con valores únicos.
