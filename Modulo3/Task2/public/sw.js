3ebd837154a40c6826cdb730ab39760a3692e143
brankohbk <brankoh@gmail.com>
2019-07-15 19:44:21 -0300

    .

--- Modulo3/Task2/public/sw.js
+++ Modulo3/Task2/public/sw.js
@@ -1,6 +1,6 @@
 importScripts('js/cache-polyfill.js');
 
-// Este service worker cachea todos los recursos para no generar trafico en la red y convierte a la PWA en instalable
+// Este service worker cachea todos los recursos para no generar trafico en la red y convierte a la PWA en instalable.
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
@@ -20,6 +20,7 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
   event.respondWith(
+    // si la info esta cacheada, muestra eso, caso contrario realiza el fetch al server.
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
